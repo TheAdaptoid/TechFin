3d4e29dad489e4128c783ef744b705ec960fcb1c
AryanG2003 <aryanshirmohammadi8@gmail.com>
2023-10-07 17:02:00 -0400

    k

--- Calculations.py
+++ Calculations.py
@@ -9,36 +9,37 @@ def Calc_Anual_Income_After_Tax(weeklyIncome:float):
     taxedAnualIncome = 0
     if(grossAnualIncome >= 578126):
         amountTaxed = grossAnualIncome - 578126
-        taxedAnualIncome = amountTaxed * .37
+        taxedAnualIncome = (amountTaxed * .37)
         grossAnualIncome - amountTaxed
     if(grossAnualIncome >= 231251):
         amountTaxed = grossAnualIncome - 231251
-        taxedAnualIncome += amountTaxed * .35
+        taxedAnualIncome = taxedAnualIncome + (amountTaxed * .35)
         grossAnualIncome - amountTaxed
     if(grossAnualIncome >= 182101):
         amountTaxed = grossAnualIncome - 182101
-        taxedAnualIncome += amountTaxed * .32
+        taxedAnualIncome = taxedAnualIncome + (amountTaxed * .32)
         grossAnualIncome - amountTaxed
     if(grossAnualIncome >= 95376):
         amountTaxed = grossAnualIncome - 95376
-        taxedAnualIncome += amountTaxed * .24
+        taxedAnualIncome = taxedAnualIncome + (amountTaxed * .24)
         grossAnualIncome - amountTaxed
     if(grossAnualIncome >= 44726):
         amountTaxed = grossAnualIncome - 44726
-        taxedAnualIncome += amountTaxed * .22
+        taxedAnualIncome = taxedAnualIncome + (amountTaxed * .22)
         grossAnualIncome - amountTaxed 
     if(grossAnualIncome >= 11001):
         amountTaxed = grossAnualIncome - 11001
-        taxedAnualIncome += amountTaxed * .12
+        taxedAnualIncome = taxedAnualIncome + (amountTaxed * .12)
         grossAnualIncome - amountTaxed
     if(grossAnualIncome >= 0):
         amountTaxed = grossAnualIncome
-        taxedAnualIncome += amountTaxed * .1
-        grossAnualIncome - amountTaxed
+        taxedAnualIncome = taxedAnualIncome + (amountTaxed * .1)
+        taxedAnualIncome = grossAnualIncome - taxedAnualIncome
     return taxedAnualIncome
 
-def Calc_Weekly_Income_After_Tax(weeklyIncome:float):
-    anualTaxedIncome = Calc_Anual_Income_After_Tax(weeklyIncome)
-    anualTaxedIncome /= 52
-    return anualTaxedIncome
+def Calc_Weekly_Income_After_Tax(anualIncome:float):
+    weeklyTaxedIncome = Calc_Anual_Income_After_Tax(anualIncome)
+    weeklyTaxedIncome /= 52
+    return weeklyTaxedIncome
 
+print(f"{Calc_Anual_Income_After_Tax(100.0)}")
