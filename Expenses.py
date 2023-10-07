062b438605e0d2cb4b2f1a116bfcfe3d355ce91c
TheAdaptoid <mhwkmn22@gmail.com>
2023-10-07 17:34:00 -0400

    All changes

--- Expenses.py
+++ Expenses.py
@@ -1,5 +1,17 @@
-def Expense(self, expenseName:str, enpenseCategory:str, expenseAmount:str):
-    self.expenseName
-    self.expenseCategory
-    self.expenseAmount
-    self.expenseFrequency
+def Expense(self, expenseName:str, expenseCategory:str, expenseAmount:float, expenseFrequency:str):
+    self.expenseName = expenseName
+    self.expenseCategory = expenseCategory
+    self.expenseAmount = expenseAmount
+    self.expenseFrequency = expenseFrequency
+
+def Get_Name(self):
+    return str(self.expenseName)
+
+def Get_Category(self):
+    return str(self.expenseCategory)
+
+def Get_Amount(self):
+    return float(self.expenseAmount)
+
+def Get_Frequency(self):
+    return str(self.expenseFrequency)
