6ca5a54acb99b29d243116a677f4bfe7d1b346ad
AryanG2003 <aryanshirmohammadi8@gmail.com>
2023-10-07 16:50:50 -0400

    k

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
     weeklyHours = int(input("How many hours do you work per week: "))
 
     grossWeeklyIncome = Calculations.Calc_Weekly_Income(hourlyRate, weeklyHours)
-    taxedWeeklyIncome = Calculations.Calc_Weekly_Income_After_Tax(Calculations.Calc_Weekly_Income(hourlyRate, weeklyHours))
-    
+    taxedAnualIncome =Calculations.Calc_Anual_Income_After_Tax(grossWeeklyIncome)
+    taxedWeeklyIncome = Calculations.Calc_Weekly_Income_After_Tax(taxedAnualIncome)
     #send to gpt
     print(f"You make ${grossWeeklyIncome} per week.")
-    print(f"You make ${taxedWeeklyIncome} per week after tax.")
-    print(f"${Calculations.Calc_Weekly_Income_After_Tax(taxedWeeklyIncome)}")
+    print(f"You make ${round(taxedWeeklyIncome,2)} per week after tax.")
+    print(f"${taxedAnualIncome}")
 main()
