84bf94c809f1e6106c6e83999f07cfebe8bb3f4a
AryanG2003 <aryanshirmohammadi8@gmail.com>
2023-10-08 06:31:42 -0400

    bigger push

--- GUI.py
+++
@@ -1,8 +0,0 @@
-import PySimpleGUI as sg
-sg.set_options(font=('Roboto Mono', 16))
-sg.theme("DarkBlue14")
-
-def User_Name(firstName: str, lastName: str):
-    user = firstName + " " + lastName[0] + "."
-    print(f"{user}")
-    return user
