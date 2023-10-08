b78e19c11882d52f7784a6bd813437dbc53c1fba
AryanG2003 <aryanshirmohammadi8@gmail.com>
2023-10-08 06:25:21 -0400

    k

---
+++ UserInterface.py
@@ -0,0 +1,24 @@
+#Local Imports
+import Calculations
+import OpenIntegrations as OI
+import Expenses
+import PySimpleGUI as sg
+
+sg.theme("DarkBlue14")
+openningGreeting = "Hello"
+layout = [
+         [sg.Text(f"{openningGreeting}")],
+         [sg.Output(size=(70,27), key= '-IN-')],
+         [sg.Push(),sg.Button('Enter'), sg.Button('Exit'),sg.Push()]
+]
+window = sg.Window("FINance GBT", layout, size= (500,500), resizable=True)
+
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+
+    if event == "Enter":
+        print(*values)
+
+window.close()
